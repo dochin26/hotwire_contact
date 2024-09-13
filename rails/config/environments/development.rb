edf95db449d83998c65bfbf7435a0614d6f276d6
dochin26 <yuuta.2635@gmail.com>
2024-09-14 01:26:03 +0900

    add devise rubocop rspec mailer

--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -67,4 +67,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # 認証メール送信に関する設定
+  config.action_mailer.default_options = { from: "no-replay@example.com" }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :letter_opener_web
 end
