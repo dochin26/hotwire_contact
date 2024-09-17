aa26153c6bcb9dbe94dc178f0ad29a07d54966fe
dochin26 <yuuta.2635@gmail.com>
2024-09-18 07:48:02 +0900

    reset to devise settings

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  mount_devise_token_auth_for "User", at: "auth"
-  resources :users
+  # mount_devise_token_auth_for "User", at: "auth"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
