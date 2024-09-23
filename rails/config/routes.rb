e95a1f32f2108779e2e57c48b65209c7374a35e2
dochin26 <yuuta.2635@gmail.com>
2024-09-23 12:50:46 +0900

    add routes file to root session new

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-   # rootをログイン画面に設定
-   devise_scope :user do
+  # rootをログイン画面に設定
+  devise_scope :user do
     root "users/sessions#new"
   end
 
   # mount_devise_token_auth_for "User", at: "auth"
-
 end
