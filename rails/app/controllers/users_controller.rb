b7379c5a909d2de0e859a2d17b9e2e81e167e675
dochin26 <yuuta.2635@gmail.com>
2024-10-19 19:16:00 +0900

    corection code

--- rails/app/controllers/users_controller.rb
+++ rails/app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
-    def show
-        @user = User.find(params[:id])
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the PSC App!"
+      redirect_to @user
+    else
+      render "new", status: :unprocessable_entity
     end
+  end
 
-    def new
-        @user = User.new
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Welcome to the PSC App!"
-            redirect_to @user
-        else
-            render 'new', status: :unprocessable_entity
-        end
-    end
-
-    private
-
-        def user_params
-            params.require(:user).permit(:name, :email, :password,
-                                         :password_confirmation)
-        end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :home
   resources :users
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
 end
